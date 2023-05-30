e926aeb6bafea1b046353137af979cdf37cc33f1
XMVasquez <ximenavasquez3410@gmail.com>
2023-05-30 14:15:13 -0600

    newemail2

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Routes, Route, Link } from "react-router-dom";
 import React, {Component} from "react";
 
+
 //faltan editar las rutas de los componentes
 import Addlibros from "./components/add-libros.component";
 import LibrosList from "./components/libros-list.component";
+import Login from './components/login.component';
+
 
 class App extends Component {
   render() {
@@ -33,7 +36,7 @@ class App extends Component {
           <h2>Books social media</h2>
           </div>
           <Routes>
-            <Route path="/" element={<LibrosList />} />
+            <Route path="/" element={<Login />} />
             <Route path="add" element={<Addlibros />} />
             <Route path="/libros" element={<LibrosList />} />
 
